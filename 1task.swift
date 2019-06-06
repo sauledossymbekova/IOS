4aefdce6200a83424e6b508800e64f604b4d2dda
sauledossymbekova <44088023+sauledossymbekova@users.noreply.github.com>
2019-06-06 13:10:33 +0600

    Update 1task.swift

--- 1task.swift
+++ 1task.swift
@@ -1,42 +1,20 @@
 class Solution {
-    func sortedSquares(_ A: [Int]) -> [Int] {
-        var arr = A.map { (num) -> Int in
-            return num * num
+   func sortedSquares(_ A: [Int]) -> [Int] {
+    let n = A.count
+    var results = [Int?](repeating: nil, count: n)
+    var i = 0, j = n - 1, p = n - 1
+    
+    while p >= 0 {
+        if (abs(A[i]) > abs(A[j])) == true{
+            results[p] = A[i] * A[i]
+            i += 1
+        } else {
+            results[p] = A[j] * A[j]
+            j -= 1
         }
-        quickSort(&arr, 0, arr.count - 1)
-        return arr
+        p -= 1
     }
     
-    private func quickSort(_ arr: inout [Int], _ left: Int, _ right: Int) {
-        if left >= right { return }
-        let m = left + (right - left) / 2
-        let pNum = arr[m]
-        let p = partition(&arr, left, right, pNum)
-        quickSort(&arr, left, p - 1) // use p-1, not p
-        quickSort(&arr, p, right)   // use p, not p+1
-    }
-    
-    private func partition(_ arr: inout [Int], _ left: Int, _ right: Int, _ pivot: Int) -> Int {
-        var l = left
-        var r = right
-        while l <= r {
-            while l < arr.count && arr[l] < pivot { l += 1 }
-            while r > 0        && arr[r] > pivot { r -= 1 }
-            if l <= r {
-                swap(&arr, l, r)
-                l += 1
-                r -= 1
-            }
-        }
-        return l
-    }
-    
-    private func swap(_ arr: inout [Int], _ a: Int, _ b: Int) {
-        if a < 0 || b < 0 || a >= arr.count || b >= arr.count {
-            return
-        }
-        let tmp = arr[a]
-        arr[a] = arr[b]
-        arr[b] = tmp
-    }
+    return results as! [Int]
+}
 }
