e4c312406a5d99b22182f02af754106e7a0545c3
sauledossymbekova <44088023+sauledossymbekova@users.noreply.github.com>
2019-06-06 13:25:14 +0600

    Update 5task.swift

--- 5task.swift
+++ 5task.swift
@@ -1,9 +1,18 @@
- let sorted = nums.sorted()
-        var sum: Int = 0
-
-        for i in stride(from: 0, to: nums.count - 1, by: 2) {
-            print(sorted[i])
-            sum += sorted[i]
+class Solution {
+    func arrayPairSum(_ nums: [Int]) -> Int {
+        if (nums.count == 0 || nums.count % 2 != 0){
+            return 0
         }
-
+        let sortedArray = nums.sorted{
+            (a:Int,b:Int) -> Bool in
+            return a < b
+        }
+        var sum = 0
+        var i = 0
+        while(i < sortedArray.count - 1) {
+            sum = sum + min(sortedArray[i],sortedArray[i+1])
+            i = i + 2
+        }
         return sum
+    }
+}
